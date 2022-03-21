1f28f967ffd08bc4e78575a2be1475d9b0a0a39c
ameer1992 <65685148+ameer1992@users.noreply.github.com>
2022-03-21 22:02:41 +0200

    Answer Question 1

--- Question1.py
+++ Question1.py
@@ -1,18 +1,16 @@
-##Question 1:
+def reverse_docorator(func):
+    def wrapper(* args, **kwargs):
+        reversed_args = args[::-1]
+        return func(*reversed_args, **kwargs)
+    return wrapper
 
-	def reverse_docorator(func):
-	    def wrapper(* args, **kwargs):
-	        reversed_args = args[::-1]
-	        return func(*reversed_args, **kwargs)
-	    return wrapper
+@reverse_docorator
+def sub(a,b):
+    return a -b 
 
-	@reverse_docorator
-	def sub(a,b):
-	    return a -b 
+print (sub(1, 2))
 
-	print (sub(1, 2))
+print (sub(a=1, b=2))
 
-	print (sub(a=1, b=2))
-
-	print (sub(1, b=2))
+print (sub(1, b=2))
 
