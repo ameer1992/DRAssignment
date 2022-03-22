54e310b224cfc9fe0aa08fa565ddba422b9172af
ameer1992 <65685148+ameer1992@users.noreply.github.com>
2022-03-22 14:38:15 +0200

    Answer Question 2

--- Question2.py
+++ Question2.py
@@ -3,15 +3,18 @@ def random_number():
     return random.randint(0, 100000000)
 
 def solve_formula(formula, n=100):
+    compiled_formula = compile(formula, '', 'eval')
     
-    effctive_formula = formula.replace('A', str(random_number())).replace('B', str(random_number())).replace('C', str(random_number()))
+    for _i in range(n):
+        
+        A, B, C = random_number(), random_number(), random_number()
+        
+        evaluted_formula = eval(compiled_formula)
+        
+        effctive_formula = formula.replace('A', str(A)).replace('B', str(B)).replace('C', str(C))
 
-    print (effctive_formula, '=' , eval(effctive_formula))
+        print (effctive_formula, '=' , evaluted_formula)
 
-    if n==1:
-        return 
-    
-    return solve_formula(formula, n-1)
-    
+    return 
 
 solve_formula("2*A+B")
